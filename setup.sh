3632c89c4bc2c394f7e07eaec61f52e9f6b7ec66
Neil Matatall <448516+oreoshake@users.noreply.github.com>
2021-11-14 19:37:44 +0000

    ensure rbenv-default-gems is installed

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-history-substring-search.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-docker.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-docker
+git clone https://github.com/rbenv/rbenv-default-gems.git $(rbenv root)/plugins/rbenv-default-gems
 
 cp .zshrc ~
 cp -R .rbenv ~
